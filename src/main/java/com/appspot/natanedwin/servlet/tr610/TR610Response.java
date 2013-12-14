c60c7e898ece12f5531724fa1a08c68096d97fc4
Bart Prokop <prokop.bart@gmail.com>
2013-12-14 18:25:35 +0100

    Denying cards of inactive people

---
+++ src/main/java/com/appspot/natanedwin/servlet/tr610/TR610Response.java
@@ -0,0 +1,114 @@
+package com.appspot.natanedwin.servlet.tr610;
+
+import com.appspot.natanedwin.entity.Device;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public final class TR610Response {
+
+    private static final String CMD_ESC = "~X";
+    private static final String CMD_DEL = "|";
+    private static final String CMD_END = CMD_DEL + "`";
+
+    public static void heartBeat1(PrintWriter resp, Device device) {
+        SimpleDateFormat dateFormater = new SimpleDateFormat("yyMMdd HHmmss");
+        dateFormater.setTimeZone(TimeZone.getTimeZone("CET"));
+        resp.print(CMD_ESC + "HeartBeat1" + CMD_DEL + dateFormater.format(new Date()) + CMD_END);
+    }
+
+    public static void hello1(PrintWriter resp, Device device) {
+        resp.print(CMD_ESC + "Hello1" + CMD_DEL + device.getId() + CMD_END);
+    }
+
+    public static void display1(PrintWriter resp, String line1) {
+        line1 = latinize(line1);
+        resp.print(CMD_ESC + "Display1" + CMD_DEL + line1 + CMD_END);
+    }
+
+    public static void display2(PrintWriter resp, String line1, String line2) {
+        line1 = latinize(line1);
+        line2 = latinize(line2);
+        resp.print(CMD_ESC + "Display2" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_END);
+    }
+
+    public static void display3(PrintWriter resp, String line1, String line2, String line3) {
+        line1 = latinize(line1);
+        line2 = latinize(line2);
+        line3 = latinize(line3);
+        resp.print(CMD_ESC + "Display3" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_DEL + line3 + CMD_END);
+    }
+
+    public static void display4(PrintWriter resp, String line1, String line2, String line3, String line4) {
+        line1 = latinize(line1);
+        line2 = latinize(line2);
+        line3 = latinize(line3);
+        line4 = latinize(line4);
+        resp.print(CMD_ESC + "Display4" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_DEL + line3 + CMD_DEL + line4 + CMD_END);
+    }
+
+    private static String latinize(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case 'Ą':
+                    c = 'A';
+                    break;
+                case 'ą':
+                    c = 'a';
+                    break;
+                case 'Ć':
+                    c = 'C';
+                    break;
+                case 'ć':
+                    c = 'ć';
+                    break;
+                case 'Ę':
+                    c = 'E';
+                    break;
+                case 'ę':
+                    c = 'ę';
+                    break;
+                case 'Ł':
+                    c = 'L';
+                    break;
+                case 'ł':
+                    c = 'l';
+                    break;
+                case 'Ń':
+                    c = 'N';
+                    break;
+                case 'ń':
+                    c = 'n';
+                    break;
+                case 'Ó':
+                    c = 'o';
+                    break;
+                case 'ó':
+                    c = 'o';
+                    break;
+                case 'Ś':
+                    c = 'S';
+                    break;
+                case 'ś':
+                    c = 's';
+                    break;
+                case 'Ż':
+                    c = 'Z';
+                    break;
+                case 'ż':
+                    c = 'z';
+                    break;
+                case 'Ź':
+                    c = 'z';
+                    break;
+                case 'ź':
+                    c = 'z';
+                    break;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
--- src/main/java/com/appspot/natanedwin/servlet/tr610/TR610Servlet.java
+++ src/main/java/com/appspot/natanedwin/servlet/tr610/TR610Servlet.java
@@ -1,6 +1,7 @@
 package com.appspot.natanedwin.servlet.tr610;
 
 import com.appspot.natanedwin.dao.DeviceDao;
+import com.appspot.natanedwin.dao.HumanDao;
 import com.appspot.natanedwin.dao.RfidCardDao;
 import com.appspot.natanedwin.dao.RfidEventDao;
 import com.appspot.natanedwin.entity.Device;
@@ -12,26 +13,32 @@ import com.appspot.natanedwin.entity.RfidEvent;
 import com.appspot.natanedwin.entity.RfidEventType;
 import com.appspot.natanedwin.service.cardnumber.CardNumber;
 import com.appspot.natanedwin.service.ofy.Ofy;
-import com.appspot.natanedwin.service.spring.SpringContext;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 
 /**
  *
  * @author prokob01
  */
+@Configurable
 public class TR610Servlet extends HttpServlet {
 
-    private static final String CMD_ESC = "~X";
-    private static final String CMD_DEL = "|";
-    private static final String CMD_END = CMD_DEL + "`";
+    @Autowired
+    private CardNumber cardNumber;
+    @Autowired
+    private DeviceDao deviceDao;
+    @Autowired
+    private HumanDao humanDao;
+    @Autowired
+    private RfidCardDao rfidCardDao;
+    @Autowired
+    private RfidEventDao rfidEventDao;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -44,7 +51,6 @@ public class TR610Servlet extends HttpServlet {
             resp.sendError(500, e.toString());
             return;
         }
-        DeviceDao deviceDao = SpringContext.INSTANCE.getBean(DeviceDao.class);
 
         Device device = deviceDao.findBySerialNumber(serialNumber);
         if (device == null) {
@@ -61,34 +67,37 @@ public class TR610Servlet extends HttpServlet {
                 cardDetected1(resp.getWriter(), device, req.getParameter("cn"), RfidEventType.valueOf(req.getParameter("mode")));
                 break;
             case HeartBeat1:
-                heartBeat1(resp.getWriter(), device);
+                TR610Response.heartBeat1(resp.getWriter(), device);
                 break;
             case Hello1:
-                hello1(resp.getWriter(), device);
+                TR610Response.hello1(resp.getWriter(), device);
                 break;
         }
     }
 
     private void cardDetected1(PrintWriter resp, Device device, String serialNumber, RfidEventType mode) {
-        RfidCardDao rfidCardDao = SpringContext.INSTANCE.getBean(RfidCardDao.class);
         RfidCard rfidCard = rfidCardDao.findBySerialNumber(serialNumber);
         if (rfidCard == null) {
             rfidCard = new RfidCard();
             rfidCard.setRfidCardType(RfidCardType.Mifare1k);
             rfidCard.setSerialNumber(serialNumber);
-            CardNumber cardNumber = SpringContext.INSTANCE.getBean(CardNumber.class);
             rfidCard.setCardNumber(cardNumber.generate());
             rfidCardDao.save(rfidCard);
-            display2(resp, "Dodano nowa karte", rfidCard.getCardNumber());
+            TR610Response.display2(resp, "Dodano nowa karte", rfidCard.getCardNumber());
             return;
         }
 
         if (rfidCard.getHuman() == null) {
-            display2(resp, "Karta nieprzypisana", rfidCard.getCardNumber());
+            TR610Response.display2(resp, "Karta nieprzypisana", rfidCard.getCardNumber());
+            return;
+        }
+        Human human = humanDao.byId(rfidCard.getHuman().getKey().getId());
+
+        if (!human.isActive()) {
+            TR610Response.display1(resp, "Osoba nieaktywna !!!");
             return;
         }
 
-        RfidEventDao rfidEventDao = SpringContext.INSTANCE.getBean(RfidEventDao.class);
         RfidEvent rfidEvent = new RfidEvent();
         rfidEvent.setDevice(device);
         rfidEvent.setRfidCard(rfidCard);
@@ -96,7 +105,7 @@ public class TR610Servlet extends HttpServlet {
         rfidEvent.setRfidEventType(mode);
         rfidEventDao.save(rfidEvent);
 
-        String line1 = "---";
+        String line1;
         switch (mode) {
             case In:
                 line1 = "Witaj";
@@ -110,117 +119,12 @@ public class TR610Servlet extends HttpServlet {
             case TempOut:
                 line1 = "Do zobaczenia pozniej";
                 break;
+            default:
+                line1 = "---";
+                break;
         }
-        String line2;
-        try {
-            Ofy ofy = SpringContext.INSTANCE.getBean(Ofy.class);
-            Human human = new Human();
-            human.setId(rfidCard.getHuman().getKey().getId());
-            human = ofy.ofy().load().entity(human).now();
-            line2 = human.getName();
-        } catch (Throwable r) {
-            line2 = r.getClass().getSimpleName();
-        }
-        display3(resp, line1, line2, rfidCard.getCardNumber());
+        String line2 = human.getName();
+        TR610Response.display3(resp, line1, line2, rfidCard.getCardNumber());
     }
 
-    private void heartBeat1(PrintWriter resp, Device device) {
-        SimpleDateFormat dateFormater = new SimpleDateFormat("yyMMdd HHmmss");
-        dateFormater.setTimeZone(TimeZone.getTimeZone("CET"));
-        resp.print(CMD_ESC + "HeartBeat1" + CMD_DEL + dateFormater.format(new Date()) + CMD_END);
-    }
-
-    private void hello1(PrintWriter resp, Device device) {
-        resp.print(CMD_ESC + "Hello1" + CMD_DEL + device.getId() + CMD_END);
-    }
-
-    private void display1(PrintWriter resp, String line1) {
-        line1 = latinize(line1);
-        resp.print(CMD_ESC + "Display1" + CMD_DEL + line1 + CMD_END);
-    }
-
-    private void display2(PrintWriter resp, String line1, String line2) {
-        line1 = latinize(line1);
-        line2 = latinize(line2);
-        resp.print(CMD_ESC + "Display2" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_END);
-    }
-
-    private void display3(PrintWriter resp, String line1, String line2, String line3) {
-        line1 = latinize(line1);
-        line2 = latinize(line2);
-        line3 = latinize(line3);
-        resp.print(CMD_ESC + "Display3" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_DEL + line3 + CMD_END);
-    }
-
-    private void display4(PrintWriter resp, String line1, String line2, String line3, String line4) {
-        line1 = latinize(line1);
-        line2 = latinize(line2);
-        line3 = latinize(line3);
-        line4 = latinize(line4);
-        resp.print(CMD_ESC + "Display4" + CMD_DEL + line1 + CMD_DEL + line2 + CMD_DEL + line3 + CMD_DEL + line4 + CMD_END);
-    }
-
-    private String latinize(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) {
-            switch (c) {
-                case 'Ą':
-                    c = 'A';
-                    break;
-                case 'ą':
-                    c = 'a';
-                    break;
-                case 'Ć':
-                    c = 'C';
-                    break;
-                case 'ć':
-                    c = 'ć';
-                    break;
-                case 'Ę':
-                    c = 'E';
-                    break;
-                case 'ę':
-                    c = 'ę';
-                    break;
-                case 'Ł':
-                    c = 'L';
-                    break;
-                case 'ł':
-                    c = 'l';
-                    break;
-                case 'Ń':
-                    c = 'N';
-                    break;
-                case 'ń':
-                    c = 'n';
-                    break;
-                case 'Ó':
-                    c = 'o';
-                    break;
-                case 'ó':
-                    c = 'o';
-                    break;
-                case 'Ś':
-                    c = 'S';
-                    break;
-                case 'ś':
-                    c = 's';
-                    break;
-                case 'Ż':
-                    c = 'Z';
-                    break;
-                case 'ż':
-                    c = 'z';
-                    break;
-                case 'Ź':
-                    c = 'z';
-                    break;
-                case 'ź':
-                    c = 'z';
-                    break;
-            }
-            sb.append(c);
-        }
-        return sb.toString();
-    }
 }
