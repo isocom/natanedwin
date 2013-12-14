2eeb24f820b5ff7af210b99ebd4e2ced3112e0bc
Bart Prokop <prokop.bart@gmail.com>
2013-12-14 14:27:13 +0100

    Fixing entityList is empty, when assiging user account to Establishment

--- src/main/java/com/appspot/natanedwin/app/menu/superadmin/ShowEstablishments.java
+++ src/main/java/com/appspot/natanedwin/app/menu/superadmin/ShowEstablishments.java
@@ -14,6 +14,7 @@ import com.appspot.natanedwin.vaadin.entity.UserAccountItem;
 import com.googlecode.objectify.cmd.Query;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.Notification;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,44 +24,48 @@ import java.util.Map;
  * @author prokob01
  */
 public class ShowEstablishments implements MenuBar.Command {
-
+    
     private final Map<String, EntityAction> additionalActions = new LinkedHashMap<>();
-
+    
     public ShowEstablishments() {
         additionalActions.put("Przypisz użytkownika", new AssignUserAccountToEstablishment1());
     }
-
+    
     @Override
     public void menuSelected(MenuItem selectedItem) {
         Ofy ofy = SpringContext.INSTANCE.getBean(Ofy.class);
         Query<Establishment> query = ofy.ofy().load().type(Establishment.class);
         List<Establishment> list = query.list();
         ofy.ofy().clear();
-
+        
         EntityContainerWindow.showWindow(new EntityContainer<>(list, EstablishmentItem.class), additionalActions);
     }
-
+    
     private class AssignUserAccountToEstablishment1 implements EntityAction<Establishment> {
-
+        
         @Override
         public void execute(Establishment entity) {
             UserAccountDao userAccountDao = SpringContext.INSTANCE.getBean(UserAccountDao.class);
             List<UserAccount> userAccounts = userAccountDao.findEstablishmentUnassigned();
-
+            if (userAccounts.isEmpty()) {
+                Notification.show("Wszystkie konta użytkowników są już przypisane !!!");
+                return;
+            }
+            
             EntityContainer<UserAccount> entityContainer = new EntityContainer<>(userAccounts, UserAccountItem.class);
             EntityItemSelectWindow.showWindow(entityContainer, new AssignUserAccountToEstablishment2(entity));
         }
-
+        
     }
-
+    
     private class AssignUserAccountToEstablishment2 implements EntityAction<UserAccount> {
-
+        
         private final Establishment establishment;
-
+        
         public AssignUserAccountToEstablishment2(Establishment establishment) {
             this.establishment = establishment;
         }
-
+        
         @Override
         public void execute(UserAccount userAccount) {
             UserAccountDao userAccountDao = SpringContext.INSTANCE.getBean(UserAccountDao.class);
