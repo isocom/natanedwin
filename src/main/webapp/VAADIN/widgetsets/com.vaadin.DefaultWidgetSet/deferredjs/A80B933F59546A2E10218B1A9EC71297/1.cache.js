61c57ccc4b0e47de40e42b7e1e73c3816fdc24d8
Bart Prokop <prokop.bart@gmail.com>
2014-02-17 06:08:30 +0100

    Adding VAADIN static folder

---
+++ src/main/webapp/VAADIN/widgetsets/com.vaadin.DefaultWidgetSet/deferredjs/A80B933F59546A2E10218B1A9EC71297/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_vaadin_DefaultWidgetSet.runAsyncCallback1('v(2648,1,Ps);_.ec=function(){uX((!dR&&(dR=new eR),dR),this.b.e)};dv(B8)(1);\n//@ sourceURL=1.js\n')
---
+++ src/main/webapp/VAADIN/widgetsets/com.vaadin.DefaultWidgetSet/deferredjs/D2333279C78DC42BCA9163960B12AD96/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_vaadin_DefaultWidgetSet.runAsyncCallback1('v(2654,1,Os);_.dc=function(){vX((!dR&&(dR=new eR),dR),this.a.d)};Zu(B8)(1);\n//@ sourceURL=1.js\n')
