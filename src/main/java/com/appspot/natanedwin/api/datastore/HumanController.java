0253c2c892c13ac48c92a187b914f57594a835b0
Bart Prokop <prokop.bart@gmail.com>
2014-02-17 06:06:03 +0100

    Adding first API classes

---
+++ src/main/java/com/appspot/natanedwin/api/datastore/HumanController.java
@@ -0,0 +1,111 @@
+package com.appspot.natanedwin.entity;
+
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ *
+ * @author prokob01
+ */
+@Entity
+@Cache
+public class Human implements Serializable, Comparable<Human> {
+
+    @Id
+    private Long id;
+    @Index
+    private String uuid = UUID.randomUUID().toString();
+    @Index
+    private Date firstTimeSeen = new Date();
+    @Index
+    private String name = "";
+    private boolean active = true;
+    private long monthlyRate = 300;
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Standard EQUALLS and HASHCODE ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    @Override
+    public int compareTo(Human o) {
+        return name.compareTo(o.name);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj != null && obj instanceof Human) {
+            return uuid.equalsIgnoreCase(((Human) obj).uuid);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return uuid.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // GETTERS AND SETTERS /////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getMonthlyRate() {
+        return monthlyRate;
+    }
+
+    public void setMonthlyRate(long monthlyRate) {
+        this.monthlyRate = monthlyRate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Date getFirstTimeSeen() {
+        return firstTimeSeen;
+    }
+
+    public void setFirstTimeSeen(Date firstTimeSeen) {
+        this.firstTimeSeen = firstTimeSeen;
+    }
+
+}
---
+++ src/main/java/com/appspot/natanedwin/api/datastore/RfidEventController.java
@@ -0,0 +1,164 @@
+package com.appspot.natanedwin.entity;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.NotFoundException;
+import com.googlecode.objectify.Ref;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
+import com.googlecode.objectify.annotation.Unindex;
+import com.googlecode.objectify.condition.IfNotNull;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ *
+ * @author prokob01
+ */
+@Entity
+public class RfidEvent implements Serializable {
+
+    @Id
+    private Long id;
+    @Unindex
+    private RfidEventType rfidEventType;
+    @Index
+    private Date eventDate = new Date();
+    @Index(IfNotNull.class)
+    private Ref<Device> device;
+    @Index(IfNotNull.class)
+    private Ref<RfidCard> rfidCard;
+    @Index(IfNotNull.class)
+    private Ref<Human> human;
+
+    @Override
+    public String toString() {
+        return getClass() + ":" + id + "/" + rfidEventType + "/" + eventDate + "/" + rfidCard;
+    }
+    ////////////////////////////////////////////////////////////////////////////
+    // Standard EQUALLS and HASHCODE ///////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (id == null) {
+            return false;
+        }
+        if (obj != null && obj instanceof RfidEvent) {
+            return id == ((RfidEvent) obj).id;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return 0;
+        } else {
+            return id.hashCode();
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // HIDING REF<?> ///////////////////////////////////////////////////////////    
+    ////////////////////////////////////////////////////////////////////////////
+    public void setDevice(Device device) {
+        this.device = Ref.create(device);
+    }
+
+    public void setHuman(Key<Human> key) {
+        this.human = Ref.create(key);
+    }
+
+    public void setHuman(Human human) {
+        this.human = Ref.create(Key.create(Human.class, human.getId()));
+    }
+
+    public RfidCard obtainRfidCard() {
+        if (rfidCard == null) {
+            return null;
+        }
+        return rfidCard.get();
+    }
+
+    public RfidCard safeRfidCard() {
+        if (rfidCard == null) {
+            throw new NotFoundException();
+        }
+        return rfidCard.safe();
+    }
+
+    public Human obtainHuman() {
+        if (human == null) {
+            return null;
+        }
+        return human.get();
+    }
+
+    public Human safeHuman() {
+        if (human == null) {
+            throw new NotFoundException();
+        }
+        return human.safe();
+    }
+
+    public void setRfidCard(RfidCard card) {
+        this.rfidCard = Ref.create(card);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // GETTERS AND SETTERS /////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////////////////
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RfidEventType getRfidEventType() {
+        return rfidEventType;
+    }
+
+    public void setRfidEventType(RfidEventType rfidEventType) {
+        this.rfidEventType = rfidEventType;
+    }
+
+    public Date getEventDate() {
+        return eventDate;
+    }
+
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public Ref<Human> getHuman() {
+        return human;
+    }
+
+    public void setHuman(Ref<Human> human) {
+        this.human = human;
+    }
+
+    public Ref<Device> getDevice() {
+        return device;
+    }
+
+    public void setDevice(Ref<Device> device) {
+        this.device = device;
+    }
+
+    public Ref<RfidCard> getRfidCard() {
+        return rfidCard;
+    }
+
+    public void setRfidCard(Ref<RfidCard> rfidCard) {
+        this.rfidCard = rfidCard;
+    }
+
+}
